6a0b00ef08c1bacdcddf2845a4876c490940d717
Yong Liu <yong.liu@ozstrategy.com>
2016-10-27 14:52:26 +0800

    add micro service discovery eureka project.

---
+++ microservice-discovery-eureka/src/main/java/com/ly/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.ly.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by yongliu on 10/17/16.
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class, args);
+  }
+}
